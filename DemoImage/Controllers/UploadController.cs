2f076212c53817646f56798cd27239a13eda79a2
minhquanini <minhquanmessi@gmail.com>
2017-02-23 23:40:49 +0700

    demo

---
+++ DemoImage/Controllers/UploadController.cs
@@ -0,0 +1,21 @@
+﻿using DemoImage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoImage.Controllers
+{
+    public class UploadController : Controller
+    {
+        //
+        // GET: /Upload/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+	}
+}
